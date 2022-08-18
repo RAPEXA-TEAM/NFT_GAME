a4f3a30e4074f4b99f6fca3f5a69a8d103b30e88
rapexa <rapexamrs@gmail.com>
2022-08-18 02:14:47 -0700

    code execute create database

--- Server/MYSQL_DB.py
+++ Server/MYSQL_DB.py
@@ -1,3 +1,4 @@
+import sys
 import pymysql
 import CONFIG     #SERVER CONGIG
 
@@ -8,7 +9,7 @@ def connect_to_database():
                        passwd=CONFIG.MYSQL_PASS,
                        db=CONFIG.MYSQL_DATABAS)
     return(db)
-
+    
 def update_user_percentage_in_database(user,percentage):
     #TODO
     pass
@@ -21,18 +22,41 @@ def check_user_messages(user):
     #TODO
     pass
 
-"CREATE DATABASE NFT_GAME;"
+def Make_Database():
+    '''This function make database'''
+    print("[+] Connecting to MySQl Server")
+    db = connect_to_database()
+    print("[+] Connected to MySQL Server")
+    cur = db.cursor()                       
+    qury = "DROP TABLE IF EXISTS Messages;"
+    cur.execute(qury)
+    db.commit()
+    db.close()
+    db = connect_to_database()
+    cur = db.cursor()                       
+    qury = "DROP TABLE IF EXISTS Users;"
+    cur.execute(qury)
+    db.commit()
+    db.close()
+    print("[+] Drop all tables")
+    db = connect_to_database()
+    cur = db.cursor()                       
+    qury = "CREATE TABLE Messages (id INT NOT NULL AUTO_INCREMENT,user VARCHAR(260),message VARCHAR(1024), PRIMARY KEY(id));"
+    cur.execute(qury)
+    db.commit()
+    db.close()
+    print("[+] Create Messages table")
+    db = connect_to_database()
+    cur = db.cursor()                       
+    qury = "CREATE TABLE Users (id INT NOT NULL AUTO_INCREMENT,user VARCHAR(260),percentage INT, PRIMARY KEY(id));"
+    cur.execute(qury)
+    db.commit()
+    db.close()
+    print("[+] Create Users table")
+    return True
 
-"CREATE USER 'NFTUSER'@'localhost' IDENTIFIED BY 'NFTPASS';"
-
-"GRANT ALL PRIVILEGES ON NFT_GAME.* TO 'NFTUSER'@'localhost';"
-
-"USE NFT_GAME;"
-
-"DROP TABLE IF EXISTS Messages;"
-
-"DROP TABLE IF EXISTS Users;"
-
-"CREATE TABLE Messages (id INT NOT NULL AUTO_INCREMENT,user VARCHAR(260),message VARCHAR(1024), PRIMARY KEY(id));"
-
-"CREATE TABLE Users (id INT NOT NULL AUTO_INCREMENT,user VARCHAR(260),percentage INT, PRIMARY KEY(id));"
+if sys.argv[1] == "execute":
+    if Make_Database():
+        print("[+] Done!")
+    else :
+        print("[-] Error...")
